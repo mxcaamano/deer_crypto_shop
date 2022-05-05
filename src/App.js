289e1c9c653b809563921d4ae7eb8e17773effaa
mxcaamano95 <mxcaamano@outlook.com>
2022-05-04 22:36:11 -0300

    Primer commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Newcomponent from "./Newcomponent";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Newcomponent></Newcomponent>
+      <h1>Nueva app en react</h1>
     </div>
   );
 }
---
+++ src/Newcomponent.js
@@ -0,0 +1,8 @@
+function Newcomponent(){
+    return(
+        <div>
+            <h2>Este es un sub-titulo</h2>
+        </div>
+    )
+}
+export default Newcomponent;
