c56d0effb6d06af6238c94f3c2df9deceecda899
mxcaamano95 <mxcaamano@outlook.com>
2022-05-19 21:25:59 -0300

    Desafio: Contador con Boton

---
+++ src/components/ItemCount.js
@@ -0,0 +1,22 @@
+import { useState } from "react";
+
+
+const ItemCount = ({ stock, initial, onAdd}) => {
+    const [Cant, setCant] = useState(initial);
+
+    const inc = () => {
+        Cant < stock && setCant (Cant+1);
+        }
+    const dec = () => {
+        Cant > initial && setCant (Cant-1);
+        }
+    return (
+        <div className="container">
+        <button type="button" className="mb-1 py-1 btn btn-danger font-weight-bold text-white" onClick={dec}>-</button>
+        <span className="mx-3 fs-5">{Cant}</span>        
+        <button type="button" className="mb-1 py-1 btn btn-success font-weight-bold text-white" onClick={inc}>+</button>
+        {<button type="button" className="mx-4 p-2 btn-view" onClick={() => { stock >= 1 && onAdd(Cant)}}>Agregar al Carrito</button>}
+        </div>
+    );
+}
+export default ItemCount;
--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,6 +1,13 @@
+import ItemCount from './ItemCount';
 const ItemListContainer = ({greeting}) => {
+    const onAdd = (Cant) => {
+        alert(`Se agregaron ${Cant} productos al carrito`);
+    }
     return (
-        <h4 className="container mt-5 text-dark">{greeting}</h4>        
+        <>
+        <h4 className="container mt-5 text-dark">{greeting}</h4>
+        <ItemCount stock={15} initial={1} onAdd={onAdd}/>
+        </>
     );
 }
 export default ItemListContainer;
